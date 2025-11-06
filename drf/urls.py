b3921e103a51a333d685bd4dbbed19b76c27455f
AlexT1021 <alexander.torres08@inacapmail.cl>
2025-11-06 14:06:12 -0300

    Se agrego las URL de documentacion API

--- drf/urls.py
+++ drf/urls.py
@@ -17,10 +17,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from app_backend import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Documentación API",
+        default_version='v1',
+        description="API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="mi_correo@test.test"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.pagina_inicio, name='home'),
     path('app_backend/', include('app_backend.urls')),
+
+    # URL's para documentación de API
+    path('apidocs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
